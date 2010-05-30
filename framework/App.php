ffbcc4f6cb005614f22a3219616234e2bcc6210c
Phil Bayfield <phil@StorageBeast.(none)>
2010-05-30 20:17:18 +0100

    adding some initial strucutres

---
+++ framework/App.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * LiteMVC Application Class
+ * 
+ * @author Phil Bayfield
+ * @copyright 2010
+ * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * @package LiteMVC
+ */
+namespace LiteMVC;
+
+class App {
+	
+}
---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+// Set constants
+define('PATH', realpath('../'));
+define('ENVIRONMENT', getenv('ENVIRONMENT'));
+
+// Get app class
+require_once('../framework/App.php');
+
+// Run application
+$app = new LiteMVC\App();
