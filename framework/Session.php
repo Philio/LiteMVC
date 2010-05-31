1f0669b147340ac17b3f3ff35d5b6962b3100d96
Phil Bayfield <phil@bayfmail.com>
2010-05-31 21:16:59 +0100

    added session class

---
+++ framework/Session.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * LiteMVC Application Framework
+ *
+ * @author Phil Bayfield
+ * @copyright 2010
+ * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * @package LiteMVC
+ */
+namespace LiteMVC;
+
+// Namespace aliases
+use LiteMVC\Session as Session;
+
+class Session
+{
+	
+}
