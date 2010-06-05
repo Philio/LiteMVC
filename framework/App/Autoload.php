1163e3c109cd41e0cef0b8590c1f211d07452142
Phil Bayfield <phil@bayfmail.com>
2010-06-05 10:45:27 +0100

    changed callback function for autoloader for benchmarking

--- framework/App/Autoload.php
+++ framework/App/Autoload.php
@@ -25,7 +25,7 @@ class Autoload
 	 * @param string $class
 	 * @return void
 	 */
-	public function loader($class)
+	public static function loader($class)
 	{
 		// Check that a path has been set
 		if (!count(self::$_paths)) {
@@ -52,7 +52,7 @@ class Autoload
 	 */
 	public function register()
 	{
-		spl_autoload_register(array($this, 'loader'));
+		spl_autoload_register(__NAMESPACE__ . '\Autoload::loader');
 	}
 	
 	/**
@@ -62,7 +62,7 @@ class Autoload
 	 */
 	public function unregister()
 	{
-		spl_autoload_unregister(array($this, 'loader'));
+		spl_autoload_unregister(__NAMESPACE__ . '\Autoload::loader');
 	}
 
 	/**
