2cd5986f7e7923196307215cc487eff903a5f2c3
Phil Bayfield <phil@bayfmail.com>
2010-06-07 08:15:34 +0100

    added JSON view componenet

---
+++ framework/View/JSON.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * LiteMVC Application Framework
+ *
+ * @author Phil Bayfield
+ * @copyright 2010
+ * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * @package LiteMVC\View
+ * @version 0.1.0
+ */
+namespace LiteMVC\View;
+
+use LiteMVC\View as View;
+
+class JSON extends View implements View\View
+{
+
+	/**
+	 * Render as JSON format
+	 *
+	 * @return string
+	 */
+	public function render()
+	{
+		// Encode page data as JSON
+		$this->_rendered = json_encode($this->_data);
+	}
+
+}
