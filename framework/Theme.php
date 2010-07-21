07e6a75520575a8b19e2558d9a1f3e8d74ca487c
Phil Bayfield <phil@bayfmail.com>
2010-07-21 13:14:16 +0100

    add theme module

---
+++ framework/Theme.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * LiteMVC Application Framework
+ *
+ * @author Phil Bayfield
+ * @copyright 2010
+ * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
+ * @package LiteMVC
+ * @version 0.1.0
+ */
+namespace LiteMVC;
+
+class Theme
+{
+
+	public function __construct(App $app)
+	{
+		
+	}
+
+}
+?>
