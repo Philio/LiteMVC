33e094b8f6d49906b894b05fe87fa0a93ce2659c
Phil Bayfield <phil@techlightenment.com>
2010-12-04 20:04:40 +0000

    refactoring dispatcher class

--- framework/Dispatcher.php
+++ framework/Dispatcher.php
@@ -30,6 +30,29 @@ class Dispatcher {
 	protected $_request;
 
 	/**
+	 * Configuration keys
+	 *
+	 * @var string
+	 */
+	const CONF_LOGIN  = 'login';
+	const CONF_ACTION = 'action';
+	const CONF_CTRLR  = 'controller';
+	const CONF_ERR    = 'error';
+	const CONF_NA     = 'notallowed';
+	const CONF_NF     = 'notfound';
+	const CONF_EXP    = 'exception';
+
+	/**
+	 * Resource names
+	 *
+	 * @var string
+	 */
+	const RES_REQUEST = 'Request';
+	const RES_AUTH    = 'Authenticate';
+	const RES_HTML    = 'View\HTML';
+	const RES_JSON    = 'View\JSON';
+
+	/**
 	 * Constructor
 	 *
 	 * @param App $app
@@ -39,7 +62,7 @@ class Dispatcher {
 	{
 		// Assign app, config and request to class vars
 		$this->_app = $app;
-		$this->_request = $app->getResource('Request');
+		$this->_request = $app->getResource(self::RES_REQUEST);
 	}
 
 	/**
@@ -53,45 +76,52 @@ class Dispatcher {
 		$module = $this->_request->getModule();
 		$controller = $this->_request->getController();
 		$action = $this->_request->getAction();
+
 		// Try and dispatch the request
 		$dispatched = false;
 		$notfound = false;
 		$exception = false;
+
 		while (!$dispatched) {
 			try {
 				// Authenticate module checks
-				if ($this->_app->isResource('Authenticate')) {
-					$auth = $this->_app->getResource('Authenticate');
+				if ($this->_app->isResource(self::RES_AUTH)) {
+					$auth = $this->_app->getResource(self::RES_AUTH);
+
 					// Check if user is allowed to view this page
 					if (!$auth->isAllowed($module, $controller, $action)) {
 						// Redirect to login
 						if ($auth->hasUserModel() && !$auth->isLoggedIn()) {
-							$login = $this->_request->getConfig('login');
+							$login = $this->_request->getConfig(self::CONF_LOGIN);
 							if (is_null($login)) {
 								throw new App\Exception('Unable to redirect to login page, configuration is invalid.');
 							}
-							$this->_request->redirect($login['action'], $login['controller']);
+							$this->_request->redirect($login[self::CONF_ACTION], $login[self::CONF_CTRLR]);
+
 						} else {
 							// Display access denied page
-							$error = $this->_request->getConfig('error');
-							if (!is_null($error) && isset($error['controller']) && isset($error['notallowed'])) {
-								$controller = $error['controller'];
-								$action = $error['notallowed'];
+							$error = $this->_request->getConfig(self::CONF_ERR);
+							if (!is_null($error) && isset($error[self::CONF_CTRLR]) && isset($error[self::CONF_NA])) {
+								$controller = $error[self::CONF_CTRLR];
+								$action = $error[self::CONF_NA];
 							} else {
 								throw new App\Exception('Access denied. Unable to load error page, configuration is invalid.');
 							}
 						}
 					}
 				}
+
 				// Get class and action names
 				$class = $this->fmtClass($module, $controller);
 				$method = $this->fmtAction($action);
+
 				// Try and load the class
 				if (class_exists($class)) {
 					$c = new $class($this->_app, $controller, $action);
 				} else {
 					throw new App\Exception('Unable to load controller class for ' . $controller . '.');
 				}
+
 				// Try and load the action
 				if (method_exists($c, $method)) {
 					// Call controller init function first if it exists
@@ -103,34 +133,38 @@ class Dispatcher {
 				} else {
 					throw new App\Exception('Unable to load action method for  ' . $action . '.');
 				}
+
 				// Dispatched ok
 				$dispatched = true;
+
 			} catch (App\Exception $e) {
 				// Page not found, display error page
 				if ($notfound) {
 					throw new App\Exception('Page not found. Unable to load error page, configuration is invalid.');
 				}
 				$notfound = true;
-				$error = $this->_request->getConfig('error');
-				if (isset($error['controller']) && isset($error['notfound'])) {
-					$controller = $error['controller'];
-					$action = $error['notfound'];
+				$error = $this->_request->getConfig(self::CONF_ERR);
+				if (isset($error[self::CONF_CTRLR]) && isset($error[self::CONF_NF])) {
+					$controller = $error[self::CONF_CTRLR];
+					$action = $error[self::CONF_NF];
 				}
+
 			} catch (\Exception $e) {
 				// Catch any other exceptions within the application
 				if ($exception) {
 					throw new App\Exception('Uncaught exception. Unable to load error page, configuration is invalid.');
 				}
 				$exception = true;
-				$error = $this->_request->getConfig('error');
-				if (isset($error['controller']) && isset($error['exception'])) {
-					$controller = $error['controller'];
-					$action = $error['exception'];
+				$error = $this->_request->getConfig(self::CONF_ERR);
+				if (isset($error[self::CONF_CTRLR]) && isset($error[self::CONF_EXP])) {
+					$controller = $error[self::CONF_CTRLR];
+					$action = $error[self::CONF_EXP];
 					// Push error message into view for display
-					if ($this->_app->isResource('View\HTML')) {
-						$this->_app->getResource('View\HTML')->exception = $e;
-					} elseif ($this->_app->isResource('View\JSON')) {
-						$this->_app->getResource('View\JSON')->exception = $e;
+					if ($this->_app->isResource(self::RES_HTML)) {
+						$this->_app->getResource(self::RES_HTML)->exception = $e;
+					}
+					if ($this->_app->isResource(self::RES_JSON)) {
+						$this->_app->getResource(self::RES_JSON)->exception = $e;
 					}
 				}
 			}
