39185999b9a5c49016c4eb72f12b7ae9514cd0ba
Phil Bayfield <phil@bayfmail.com>
2012-05-18 16:47:56 +0100

    phpdoc corrections

--- framework/REST.php
+++ framework/REST.php
@@ -5,7 +5,8 @@
  * @author Phil Bayfield
  * @copyright 2012
  * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
- * @package Api
+ * @package LiteMVC
+ * @version 0.1.0
  */
 namespace LiteMVC;
 
--- framework/REST/Exception.php
+++ framework/REST/Exception.php
@@ -3,9 +3,9 @@
  * LiteMVC Application Framework
  *
  * @author Phil Bayfield
- * @copyright 2010
+ * @copyright 2012
  * @license Creative Commons Attribution-Share Alike 2.0 UK: England & Wales License
- * @package LiteMVC\Database
+ * @package LiteMVC\REST
  * @version 0.1.0
  */
 namespace LiteMVC\REST;
