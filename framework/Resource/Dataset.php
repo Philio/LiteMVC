f01c9b69b362c0068b8fc40a8cd7d2109f46d2ab
Phil Bayfield <phil@bayfmail.com>
2012-06-24 14:10:10 +0100

    add ArrayAccess and Interator to Dataset

--- framework/Resource/Dataset.php
+++ framework/Resource/Dataset.php
@@ -13,7 +13,7 @@ namespace LiteMVC\Resource;
 // Namespace aliases
 use LiteMVC\Resource as Resource;
 
-abstract class Dataset extends Resource implements \Countable
+abstract class Dataset extends Resource implements \ArrayAccess, \Iterator, \Countable
 {
 
 	/**
@@ -81,6 +81,101 @@ abstract class Dataset extends Resource implements \Countable
 			return $this->_data[$name];
 		}
 		return $default;
+	}
+
+	/**
+	 * Set an offset
+	 *
+	 * @param mixed $offset
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function offsetSet($offset, $value) {
+        if (is_null($offset)) {
+            $this->_data[] = $value;
+        } else {
+            $this->_data[$offset] = $value;
+        }
+    }
+
+	/**
+	 * Get an offset
+	 *
+	 * @param mixed $offset
+	 * @return mixed
+	 */
+	public function offsetGet($offset) {
+        return isset($this->_data[$offset]) ? $this->_data[$offset] : null;
+    }
+
+	/**
+	 * Check if an offset exists
+	 *
+	 * @param mixed $offset
+	 * @return bool
+	 */
+	public function offsetExists ($offset)
+	{
+		return isset($this->_data[$offset]);
+	}
+
+	/**
+	 * Unset an offset
+	 *
+	 * @param mixed $offset
+	 */
+	public function offsetUnset($offset) {
+        unset($this->_data[$offset]);
+    }
+
+	/**
+	 * Get current element
+	 *
+	 * @return mixed
+	 */
+	public function current()
+	{
+		return current($this->_data);
+	}
+
+	/**
+	 * Get key of the current element
+	 *
+	 * @return mixed
+	 */
+	public function key()
+	{
+		return key($this->_data);
+	}
+
+	/**
+	 * Move to next element
+	 *
+	 * @return void
+	 */
+	public function next()
+	{
+		next($this->_data);
+	}
+
+	/**
+	 * Reset internal pointer
+	 *
+	 * @return void
+	 */
+	public function rewind()
+	{
+		reset($this->_data);
+	}
+
+	/**
+	 * Is current element valid
+	 *
+	 * @return bool
+	 */
+	public function valid()
+	{
+		return current($this->_data) ? true : false;
 	}
 
 	/**
